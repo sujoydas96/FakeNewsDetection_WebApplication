9fa55bcde06c37e5e15fccc5c6b67f8a6ce665e1
Sujoy Das <sujoydas96>
2021-05-25 19:40:55 +0530

    minor change

--- Project.py
+++ Project.py
@@ -1,3 +1,8 @@
+#from simplified_scrapy import SimplifiedDoc,req,utils
+from bs4 import BeautifulSoup
+import requests
+from googlesearch import search
+
 class Backend:
   def __init__(self):
     self.results = list()
@@ -5,7 +10,7 @@ class Backend:
   
   def Search(self, Phrase):
     self.Phrase = Phrase
-    from googlesearch import search
+    
 
     for j in search(self.Phrase):
       self.results.append(j)
@@ -20,9 +25,20 @@ class Backend:
     #this function must process all the information and return a binary answer.
     #make a file in python that documents the frequency of the number of times a website appears in the searches. later in practice the websites needs to be given a greater preference if it has appeared multiple times.
     #webscrap the webpages here
-    pass
+    
+    URL = self.results[1]
+    
+    #URL = "https://www.geeksforgeeks.org/data-structures/"
+    r = requests.get(URL)
+    #print(r.content)
+
+    soup = BeautifulSoup(r.content, 'html5lib') # If this line causes an error, run 'pip install html5lib' or install html5lib
+    print(soup.prettify())
+
+    
 
 obj = Backend()
 inp = str(input("Type in Headline... "))
 obj.Search(inp)
 obj.Print_Search()
+obj.Process()
